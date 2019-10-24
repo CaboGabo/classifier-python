81715c64ffc727e0462924291deb3e9249ba5bfc
cabogabo <gabo.alejandro.huitron@gmail.com>
2019-10-24 14:14:55 -0500

    Las palabras más usadas en los posts del usuario

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import json
 from classifiers import *
+from nltk.probability import FreqDist
 
 classifiers = [getClassifier(objsa2), getClassifier(objsa3), getClassifier(objsa4), getClassifier(objsa6), getClassifier(objsa7), getClassifier(
     objsa8), getClassifier(objsa9), getClassifier(objsb1), getClassifier(objsb4), getClassifier(objsb6), getClassifier(objsc1)]
@@ -10,12 +11,24 @@ testsText = tests.read()
 testsObj = json.loads(testsText)
 
 testsObj = getTokenizedText(testsObj)
+total_words = []
 
+results = []
 for test in testsObj:
     test['text'] = removeWords(test['text'])
+    total_words += test['text']
     [stemmed] = stemming([test])
     test = stemmed
-    print('Current post', test)
+    postTags = []
     for classifier in classifiers:
         test_document = documentFeatures(test, classifier[1])
-        print(classifier[0].classify(test_document))
+        postTags.append(classifier[0].classify(test_document))
+
+    results.append([test, postTags])
+
+for result in results:
+    print(result)
+
+all_words = FreqDist(total_words)
+words_used = list(all_words.keys())[:10]
+print(words_used)
